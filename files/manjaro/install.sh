0704022b63e0518088dafc1e09b972311f7e4c3b
Valerio Ferretti <valerio.ferretti92@gmail.com>
2021-08-06 01:01:47 +0200

    Manjaro installation scripts made independent from dotfile dir path

--- files/manjaro/install.sh
+++ files/manjaro/install.sh
@@ -2,25 +2,25 @@
 
 echo " * Setting up config files for unprivileged user:"
 printf "Setting up .zshrc... "
-ln -sf ${HOME}/private-repo/dotfiles/files/arch/.zshrc ${HOME}/.zshrc
+ln -sf $(pwd)/$(dirname $0)/.zshrc ${HOME}/.zshrc
 echo "Done"
 printf "Setting up .screenrc... "
-ln -sf ${HOME}/private-repo/dotfiles/files/arch/.screenrc ${HOME}/.screenrc
+ln -sf $(pwd)/$(dirname $0)/.screenrc ${HOME}/.screenrc
 echo "Done"
 printf "Setting up init.vim... "
 mkdir -p ~/.config/nvim
-ln -sf ${HOME}/private-repo/dotfiles/files/arch/init.vim ${HOME}/.config/nvim/init.vim
+ln -sf $(pwd)/$(dirname $0)/init.vim ${HOME}/.config/nvim/init.vim
 echo "Done"
 
 USER_HOME=$HOME
 sudo echo " * Setting up config files for root:"
 printf "Setting up .zshrc..."
-sudo ln -sf ${USER_HOME}/private-repo/dotfiles/files/arch/.zshrc-root /root/.zshrc
+sudo ln -sf $(pwd)/$(dirname $0)/.zshrc-root /root/.zshrc
 echo "Done"
 printf "Setting up .screenrc... "
-sudo ln -sf ${USER_HOME}/private-repo/dotfiles/files/arch/.screenrc /root/.screenrc
+sudo ln -sf $(pwd)/$(dirname $0)/.screenrc /root/.screenrc
 echo "Done"
 printf "Setting up init.vim... "
 sudo mkdir -p /root/.config/nvim
-sudo ln -sf ${USER_HOME}/private-repo/dotfiles/files/arch/init.vim /root/.config/nvim/init.vim
+sudo ln -sf $(pwd)/$(dirname $0)/init.vim /root/.config/nvim/init.vim
 echo "Done"
