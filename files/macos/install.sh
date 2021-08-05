1ca1b241930afbc6e38a7cfd1a4a39a367fc953b
Valerio Ferretti <valerio.ferretti@symphony.com>
2021-08-06 00:49:37 +0200

    .screenrc, .zshrc, init.vim for macos env and installation scripts

---
+++ files/macos/install.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo " * Setting up config files for unprivileged user:"
+printf "Setting up .zshrc... "
+ln -sf $(pwd)/$(dirname $0)/.zshrc ${HOME}/.zshrc
+echo "Done"
+printf "Setting up .screenrc... "
+ln -sf $(pwd)/$(dirname $0)/.screenrc ${HOME}/.screenrc
+echo "Done"
+printf "Setting up init.vim... "
+mkdir -p ~/.config/nvim
+ln -sf $(pwd)/$(dirname $0)/init.vim ${HOME}/.config/nvim/init.vim
+echo "Done"
--- install.sh
+++ install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+set -e
+
 echo "Dotfiles provided for given environments:"
 echo -e "*****************************************"
-echo "	1) Arch"
+echo "	1) Manjaro"
+echo "	2) MacOS"
 echo -e "*****************************************\n"
 
 read -p "Select your environment: " ENV_INDEX
 case $ENV_INDEX in
   1)
-    cd files/arch
-    ./install.sh
-    cd ../..
+    $(dirname $0)/files/manjaro/install.sh
+    ;;
+  2)
+    $(dirname $0)/files/macos/install.sh
     ;;
   *)
-    echo "No environment selected!"
+    echo "No environment selected, bye!"
+    exit -1
     ;;
 esac
-echo "Bye"
 
 echo -e "\n\nnvim may require additional set up:"
 echo " 1) Installation of vim-plug"
 echo "    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \\"
 echo "    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
-echo " 1) Plugin installation"
+echo " 2) Plugin installation"
 echo "    Open nvim and enter the command :PlugInstall"
