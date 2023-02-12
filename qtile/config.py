0b5ecf16471ca8d21c9f50bb656970b3bc3e2f8d
Valerio Ferretti <valerio.ferretti92@gmail.com>
2023-02-12 14:23:36 +0100

    Floating Pavucontrol in Qtile config

--- qtile/config.py
+++ qtile/config.py
@@ -471,6 +471,7 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="Pavucontrol"),  # gitk
         Match(title="branchdialog"),  # gitk
         Match(title="Enpass"),  # gitk
         Match(title="pinentry")  # GPG key password entry
