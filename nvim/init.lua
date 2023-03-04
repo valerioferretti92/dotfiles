0df15d398ae46e63f79059084644718fe6c595f9
Valerio Ferretti <valerio.ferretti92@gmail.com>
2023-03-04 20:26:38 +0100

    Simplified Nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,48 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require "globals"
+require "mappings"
+
+local lazy = {}
+
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  -- You can "comment out" the line below after lazy.nvim is installed
+  -- lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+  require('lazy').setup(plugins, lazy.opts)
+end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+lazy.setup({
+	{ 
+		'ellisonleao/gruvbox.nvim' 
+	},
+	{ 
+		'nvim-lualine/lualine.nvim' 
+	},
+	{ 
+		'christoomey/vim-tmux-navigator'
+	},
+	{
+		'nvim-telescope/telescope.nvim', 
+		tag = '0.1.1', 
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+})
+
+require "plugins-config"
---
+++ nvim/lua/globals.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.background = "dark"
