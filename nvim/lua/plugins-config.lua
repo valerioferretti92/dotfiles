552c4bec30840176bcca97e855d0d799ba05286a
Valerio Ferretti <valerio.ferretti92@gmail.com>
2023-08-09 00:06:07 +0200

    NeoVim Telescope not to ignore .go files

--- nvim/lua/plugins-config.lua
+++ nvim/lua/plugins-config.lua
@@ -68,7 +68,7 @@ require("telescope").setup({
 			preview_cutoff = 120,
 		},
 		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		file_ignore_patterns = {"node_modules", ".git/", "dist/", "go"},
+		file_ignore_patterns = {"node_modules", ".git/", "dist/", "node_modules"},
 		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
 		path_display = { "absolute" },
 		winblend = 0,
