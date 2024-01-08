06231c371b8a8a010fb5551af52401b86f023781
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-01-08 19:17:24 +0100

    Neovim, treesitter indenting disabled

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -43,7 +43,7 @@ return {{
             use_languagetree = true
         },
         indent = {
-            enable = true
+            enable = false
         },
         autotag = {
             enable = true
