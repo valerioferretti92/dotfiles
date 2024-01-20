d534d2b8abe4543266ebebeaccafed67082b81f7
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-01-20 15:45:57 +0100

    Nvim config indentation fixed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -181,9 +181,13 @@ return {{
 				end
 			})
 		end
-	},
-					{"saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
-					 "hrsh7th/cmp-path"}}, -- cmp sources plugins
+		},{
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path"
+	}}, -- cmp sources plugins
 	opts = function()
 		local cmp = require "cmp"
 
