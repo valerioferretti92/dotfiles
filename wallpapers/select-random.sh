11b26e13f0a9d56f08137327f03bafa705a5b2d8
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-01-27 12:13:55 +0100

    Swww transition step set to 10

--- wallpapers/select-random.sh
+++ wallpapers/select-random.sh
@@ -2,4 +2,4 @@
 
 WALLPAPERS_PATH=${1:-$HOME/repos/public/dotfiles/wallpapers}
 WALLPAPER_NAME=$(ls ${WALLPAPERS_PATH} | shuf -n 1)
-swww img ${WALLPAPERS_PATH}/${WALLPAPER_NAME}
+swww img --transition-step 10 ${WALLPAPERS_PATH}/${WALLPAPER_NAME}
