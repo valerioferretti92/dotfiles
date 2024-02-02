74e950f9def0c8e42b744ee7bf4e3cf9e582a3a8
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-02-02 22:24:04 +0100

    Neovim, Alpha greeter

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -10,10 +10,12 @@
 -- Description: nvim-telescope config
 -- Author: Valerio Ferretti <valerio.ferretti92@gmail.com>
 return {{
-	'nvimdev/dashboard-nvim',
-	event = 'VimEnter',
-	config = function()
-		require('dashboard').setup { }
-	end,
-	dependencies = { {'nvim-tree/nvim-web-devicons'}}
+		'goolord/alpha-nvim',
+		dependencies = {
+				'nvim-tree/nvim-web-devicons',
+				'nvim-lua/plenary.nvim'
+		},
+		config = function ()
+				require'alpha'.setup(require'alpha.themes.theta'.config)
+		end
 }}
