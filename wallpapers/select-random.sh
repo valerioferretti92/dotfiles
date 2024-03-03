a9cf850703556318b0bf7aaaad3d9dfece0d531a
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-03-03 23:24:07 +0100

    Wallpaper, get monitor list and apply

--- wallpapers/select-random.sh
+++ wallpapers/select-random.sh
@@ -6,7 +6,6 @@ LOCK_FILE="/tmp/wallpaper.lock"
 
 # Function to remove lock file
 remove_lock_file() {
-		echo "Removing $LOCK_FILE..."
 		if [ -f "$LOCK_FILE" ];
 		then
 				rm -f "$LOCK_FILE"
@@ -16,17 +15,18 @@ remove_lock_file() {
 		fi
 }
 
-# Trap any signals or errors and remove lock file before exiting
-trap 'remove_lock_file' EXIT
-
 # Check if lock file exists
 if [ -f "$LOCK_FILE" ]; then
 		echo "Lock file exists. Exiting."
 		exit 0
 fi
 
+# Trap any signals or errors and remove lock file before exiting
+trap 'remove_lock_file' EXIT
+
 # Create lock file
 touch "$LOCK_FILE"
+echo "$LOCK_FILE created."
 
 WALLPAPERS_PATH=${1:-$HOME/repos/public/dotfiles/wallpapers/files}
 WALLPAPER_NAME=$(ls ${WALLPAPERS_PATH} | shuf -n 1)
@@ -34,7 +34,11 @@ WALLPAPER_NAME=$(ls ${WALLPAPERS_PATH} | shuf -n 1)
 # Hyprpaper
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "${WALLPAPERS_PATH}/${WALLPAPER_NAME}"
-hyprctl hyprpaper wallpaper "eDP-1,${WALLPAPERS_PATH}/${WALLPAPER_NAME}"
-hyprctl hyprpaper wallpaper "HDMI-A-1,${WALLPAPERS_PATH}/${WALLPAPER_NAME}"
+
+MONITORS=$(hyprctl monitors | grep Monitor | sed 's/^Monitor \(.*\) (ID [0-9]*):$/\1/')
+for MONITOR in $MONITORS
+do
+	hyprctl hyprpaper wallpaper "${MONITOR},${WALLPAPERS_PATH}/${WALLPAPER_NAME}"
+done
 
 exit 0
