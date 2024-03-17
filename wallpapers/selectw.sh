ebe0fbadcd313590d824d62eb4af42d434cbf812
Valerio Ferretti <valerio.ferretti92@gmail.com>
2024-03-17 14:35:28 +0100

    Wallpapers, added preview key binding to selectw.sh

--- wallpapers/selectw.sh
+++ wallpapers/selectw.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-WALLPAPER_PATH=$(find "$HOME/repos/public/dotfiles/wallpapers/files" -type f | fzf)
+WALLPAPER_PATH=$(find "$HOME/repos/public/dotfiles/wallpapers/files" -type f | fzf --bind='ctrl-a:execute(feh --image-bg "#1b1c16" --scale-down {} > /dev/null 2>&1 &)')
 
 if [ -z "$WALLPAPER_PATH" ]; then
 	exit -1
